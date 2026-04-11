7b6d90449ca668aaaa23f4ea23c030afdcef13ac
Nikelson-netizen <kellysense31@gmail.com>
2026-04-11 00:36:37 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   next();
 });
 
-const PUBLIC_PATH = path.join(__dirname, "www");
+const PUBLIC_PATH = path.join(__dirname, "WWW");
 
 app.use(express.static(PUBLIC_PATH));
 
