ace2dd0a3821fc212f995e1aa14064cf967b615b
Nikelson-netizen <kellysense31@gmail.com>
2026-05-17 11:49:14 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -160,7 +160,7 @@ if (chatHeader && chatContent) {
   });
 }
 
-const placeSound = new Audio("/sounds/click.mp3");
+const placeSound = new Audio("/click.mp3");
 placeSound.preload = "auto";
 
 function playPlaceSound() {
